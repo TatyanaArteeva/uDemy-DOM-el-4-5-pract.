6ca01427bf8960714d0900095523b002a0b3938c
Tatyana Arteeva <arteevata93@gmail.com>
2020-09-21 10:34:48 +0300

    Part 1 is finished

--- js/scripttt.js
+++ js/scripttt.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
     const movieDB = {
         movies: [
@@ -14,69 +14,77 @@ document.addEventListener('DOMContentLoaded', ()=>{
         ]
     };
 
-    const reclame=document.querySelector('.promo__adv');
-    const getFoto=reclame.querySelectorAll('img');
-        for (let i=0; i<getFoto.length; i++) {
+    const reclame = document.querySelector('.promo__adv');
+    const getFoto = reclame.querySelectorAll('img');
+        for (let i = 0; i < getFoto.length; i++) {
             getFoto[i].remove();
         }
 
-    const genre=document.querySelector('.promo__genre');
-    const createtGenre=document.createElement('div');
-        createtGenre.classList.add('promo__genre');
-        createtGenre.textContent="Драма";
-        genre.replaceWith(createtGenre);
+    const genre = document.querySelector('.promo__genre');
+    const createtGenre = document.createElement('div');
+    createtGenre.classList.add('promo__genre');
+    createtGenre.textContent = "Драма";
+    genre.replaceWith(createtGenre);
 
-    const foto=document.querySelector('.promo__bg');
-        foto.style.background='url(\'img/bg.jpg\') center center/cover no-repeat';
+    const foto = document.querySelector('.promo__bg');
+    foto.style.background = 'url(\'img/bg.jpg\') center center/cover no-repeat';
 
-    const form=document.querySelector('.add');
-    const btn=form.querySelector('button');
-    const input=form.querySelector('.adding__input');
-    const accommodation=document.querySelector('.promo__interactive-list');
+    const form = document.querySelector('.add');
+    const btn = form.querySelector('button');
+    const input = form.querySelector('.adding__input');
+    const accommodation = document.querySelector('.promo__interactive-list');
 
-    const addListFilms=(e)=> {
+    const addListFilms = (e) => {
         e.preventDefault();
-        let li=document.createElement('li');
-            li.classList.add('promo__interactive-item');
-        let createtLi=accommodation.appendChild(li);
-            createtLi.innerHTML=input.value;
-            movieDB.movies.push(createtLi.innerHTML);
-            movieDB.movies.sort(compSort);
-            
-        let list=document.querySelectorAll('.promo__interactive-item');
-            movieDB.movies.forEach((item,i)=> {
-                if (item.length<=21) {
-                    list[i].innerHTML= `${i+1}: ${item} <div class="delete"></div>`;
-                    deleteFilm();
-                } else {
-                    list[i].innerHTML= `${i+1}: ${item.slice(0,22)+'...'} <div class="delete"></div>`;
-                    deleteFilm();
-                }
-                })
-        
-        const lovesFilm=form.querySelector('input[type="checkbox"]');
-
-            if (lovesFilm.checked==true) {
-            alert("Добавить любимый фильм!");
-            }
-            e.target.form.reset();
+        const inputValue = input.value;
+        if (inputValue == null || inputValue.length == 0) {
+            return;
         }
+        let liNewFilm = document.createElement('li');
+        liNewFilm.classList.add('promo__interactive-item');
+        let createdLi = accommodation.appendChild(liNewFilm);
+        createdLi.innerHTML = inputValue;
+        movieDB.movies.push(createdLi.innerHTML);
+        movieDB.movies.sort(compSort);
+        
+        normalizeFilms();
+        addListenerToTrashes(movieDB.movies);
+
+        const lovesFilm = form.querySelector('input[type="checkbox"]');
+        if (lovesFilm.checked == true) {
+            alert("Добавить любимый фильм!");
+        }
+        e.target.form.reset();
+    };
+
+    normalizeFilms();
+    addListenerToTrashes(movieDB.movies);
     btn.addEventListener('click', addListFilms);
 
-
-    function deleteFilm() {
-    const trash=document.getElementsByClassName('delete');
-        let deleteStr=function(e) {
-            e.target.parentElement.remove();
-        }
-
-        for (let i=0; i<trash.length; i++) {
-            trash[i].addEventListener('click', deleteStr);
-        }
+    function normalizeFilms(){
+        let list = document.querySelectorAll('.promo__interactive-item');
+        movieDB.movies.forEach((film,num)=> {
+            if (film.length <= 21) {
+                list[num].innerHTML= `${num+1}: ${film} <div class="delete"></div>`;
+            } else {
+                list[num].innerHTML= `${num+1}: ${film.slice(0,22)+'...'} <div class="delete"></div>`;
+            }
+        });
     }
 
+    function addListenerToTrashes(listFilms) {
+        const trashes = document.querySelectorAll('.delete');
+        trashes.forEach((trash, i) => {
+            trash.addEventListener('click', () => {
+                trash.parentElement.remove();
+                listFilms.splice(i,1);
+                normalizeFilms();
+                addListenerToTrashes();
+            });
+        });
+    }
 
-    function compSort (a,b) {
+    function compSort(a, b) {
         a = a.toLowerCase();
         b = b.toLowerCase();
         if (a == b) {
@@ -87,5 +95,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
         return -1;
     }
+
 });
 
+
+
+
+
+// function deleteFilm() {
+    // const trash=document.getElementsByClassName('delete');
+    //     let deleteStr=function(e) {
+    //         e.target.parentElement.remove();
+    //     }
+
+    //     for (let i=0; i<trash.length; i++) {
+    //         trash[i].addEventListener('click', deleteStr);
+
+    //     }
+    // }
+
+// const a = function(str, number) {
+//      console.log(str + ", " + number);
+// }
+// a("Татьяна", 26);
